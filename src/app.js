c2158055b9e0f3d98b567799417d7679cf6c876e
shubam <shubhammishra4227@gmail.com>
2026-02-11 09:19:09 +0000

    added new

--- src/app.js
+++ src/app.js
@@ -3,28 +3,22 @@ const path = require('path');
 
 const indexRoutes = require('./routes/index.routes');
 const destinationRoutes = require('./routes/destinations.routes');
-const logger = require('./middleware/request-logger');
-const errorHandler = require('./middleware/error-handler');
 
 const app = express();
 
 app.use(express.json());
-app.use(logger);
 
-// STATIC FILES FIRST (important)
+// Static UI
 app.use('/ui', express.static(path.join(__dirname, 'views')));
 
-// ROUTES
+// Routes
 app.use('/', indexRoutes);
 app.use('/api/destinations', destinationRoutes);
 
-// HEALTH CHECK (must always respond)
+// Health check (App Runner)
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'UP' });
 });
-
-// ERROR HANDLER LAST
-app.use(errorHandler);
 
 module.exports = app;
 
